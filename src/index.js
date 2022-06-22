34169897093960433b3a51c56b2aaf4fcc48f047
kaito kuroba <3118659412@qq.com>
2022-06-22 15:57:56 +0800

    update: ttf zip

--- src/index.js
+++ src/index.js
@@ -3,15 +3,3 @@ import _ from 'lodash';
 import './style/style.css';
 import './js/mv.js';
 
-// import "tailwindcss/tailwind.css"
-
-// function component(){
-//   const element = document.createElement('div');
-
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-//   element.classList.add('hello');
-
-//   return element;
-// }
-// document.body.appendChild(component());
